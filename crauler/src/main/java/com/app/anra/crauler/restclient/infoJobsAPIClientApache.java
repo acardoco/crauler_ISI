a2c5f67c5587f4e89d93e9457cd027cc1bca8745
AndresCC12 <andres.cardosoc12@gmail.com>
2016-11-21 18:49:37 +0100

    arreglo al pom

--- crauler/src/main/java/com/app/anra/crauler/restclient/infoJobsAPIClientApache.java
+++ crauler/src/main/java/com/app/anra/crauler/restclient/infoJobsAPIClientApache.java
@@ -17,34 +17,31 @@ public class infoJobsAPIClientApache {
 	static String username = "5657566713114d71b71516ba9395d4ae";
 	static String password = "cmo+EdVRuqQyEwGQRV4H5J5+lITBAoTPLHr+rwLmRu5EoD7WHJ";
 
-	
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws ClientProtocolException, IOException {
-		DefaultHttpClient client = new DefaultHttpClient();   
-		  
-        HttpHost targetHost = new HttpHost("api.infojobs.net", 443, "https");   
-  
-        // Provide the credentials to be used for the host against which   
-        // authentication is to be attempted.  
-        client.getCredentialsProvider().setCredentials(  
-            new AuthScope(targetHost.getHostName(), targetHost.getPort()),        
-            new UsernamePasswordCredentials(username, password)  
-        );  
-          
-        // create a GET method that queries some API operation  
-        HttpGet request = new HttpGet("/api/1/offer");    
-  
-        try {  
-            // execute the operation  
-            HttpResponse response = client.execute(targetHost, request);  
-              
-            // print the status and the contents of the response  
-            System.out.println(response.getStatusLine());  
-            System.out.println(EntityUtils.toString(response.getEntity()));  
-        } finally {  
-            // release any connection resources used by the method  
-            client.getConnectionManager().shutdown();  
-        } 
+		DefaultHttpClient client = new DefaultHttpClient();
+
+		HttpHost targetHost = new HttpHost("api.infojobs.net", 443, "https");
+
+		// Provide the credentials to be used for the host against which
+		// authentication is to be attempted.
+		client.getCredentialsProvider().setCredentials(new AuthScope(targetHost.getHostName(), targetHost.getPort()),
+				new UsernamePasswordCredentials(username, password));
+
+		// create a GET method that queries some API operation
+		HttpGet request = new HttpGet("/api/1/offer");
+
+		try {
+			// execute the operation
+			HttpResponse response = client.execute(targetHost, request);
+
+			// print the status and the contents of the response
+			System.out.println(response.getStatusLine());
+			System.out.println(EntityUtils.toString(response.getEntity()));
+		} finally {
+			// release any connection resources used by the method
+			client.getConnectionManager().shutdown();
+		}
 
 	}
 
