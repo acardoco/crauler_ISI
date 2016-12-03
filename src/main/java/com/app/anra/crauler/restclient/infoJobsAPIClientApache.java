1f581f1b116c3223f6e1262efbe7e5ef7283f1dc
Raúl Hernáez <rhorbul@gmail.com>
2016-12-03 19:45:19 +0100

    Cliente REST de Twitter y InfoJobs

--- src/main/java/com/app/anra/crauler/restclient/infoJobsAPIClientApache.java
+++ src/main/java/com/app/anra/crauler/restclient/infoJobsAPIClientApache.java
@@ -17,13 +17,16 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import com.app.anra.crauler.model.VOs.Empresa;
 import com.app.anra.crauler.model.VOs.Oferta;
 
 public class infoJobsAPIClientApache {
 
-	static String infoJobsUrl = "https://api.infojobs.net/api/1/offer";
-	static String username = "5657566713114d71b71516ba9395d4ae";
-	static String password = "cmo+EdVRuqQyEwGQRV4H5J5+lITBAoTPLHr+rwLmRu5EoD7WHJ";
+	private static String infoJobsOffersURL = "/api/1/offer?page=";
+	private static int numPages = 1; //MAX = 114;
+	private static String infoJobsCompanyURL = "/api/1/profile/";
+	private static String username = "5657566713114d71b71516ba9395d4ae";
+	private static String password = "cmo+EdVRuqQyEwGQRV4H5J5+lITBAoTPLHr+rwLmRu5EoD7WHJ";
 
 	public static void main(String[] args) throws ClientProtocolException, IOException {
 		
@@ -34,44 +37,68 @@ public class infoJobsAPIClientApache {
 		
 		CloseableHttpClient client = HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build();
 
-		// create a GET method that queries some API operation
-		HttpGet request = new HttpGet("/api/1/offer");
+		HttpGet offersRequest = null;
+		HttpGet companyRequest = null;
+		HttpResponse response = null;
+		
+		JSONArray ofertasJSON = null;
+		JSONObject ofertaJSON = null;
+		JSONObject empresaJSON = null;
+		
+		ArrayList<Oferta> ofertas = new ArrayList<Oferta>();
+		ArrayList<Empresa> empresas = new ArrayList<Empresa>();
 
 		try {
-			// execute the operation
-			HttpResponse response = client.execute(targetHost, request);
-
-			// print the status and the contents of the response
-			System.out.println(response.getStatusLine());
+		
+		for(int page = 1; page <= numPages; page++){
 			
-			JSONObject jsonObj = new JSONObject(EntityUtils.toString(response.getEntity(), "UTF-8"));
-			JSONArray array = jsonObj.getJSONArray("offers");
-			ArrayList<Oferta> ofertas = new ArrayList<Oferta>();
+			offersRequest = new HttpGet(infoJobsOffersURL+page);
+			response = client.execute(targetHost, offersRequest);
+			ofertasJSON = (new JSONObject(EntityUtils.toString(response.getEntity(), "UTF-8")).getJSONArray("offers"));
 			
-			for(int n = 0; n < array.length(); n++)
+			System.out.println(" ----- Recogiendo ofertas de página " + page);
+			
+			for(int numOferta = 0; numOferta < ofertasJSON.length(); numOferta++)
 			{
-			    JSONObject object = array.getJSONObject(n);
+				ofertaJSON = ofertasJSON.getJSONObject(numOferta);
 			    
-				Oferta ofertilla = new Oferta();
-				ofertilla.setNombre_oferta(object.get("title").toString());
-				ofertilla.setNombre_empresa(object.getJSONObject("author").get("name").toString());
-				ofertilla.setLocalizacion(object.getJSONObject("province").get("value").toString());
-				ofertilla.setDescrp_oferta("descripción");
-				ofertilla.setExperiencia(object.getJSONObject("experienceMin").get("value").toString());
-				ofertas.add(ofertilla);
+				Oferta oferta = new Oferta();
+				oferta.setNombre_oferta(ofertaJSON.get("title").toString());
+				oferta.setNombre_empresa(ofertaJSON.getJSONObject("author").get("name").toString());
+				oferta.setLocalizacion(ofertaJSON.getJSONObject("province").get("value").toString());
+				oferta.setDescrp_oferta("descripción");
+				oferta.setExperiencia(ofertaJSON.getJSONObject("experienceMin").get("value").toString());
+				ofertas.add(oferta);
+				
+				companyRequest = new HttpGet(infoJobsCompanyURL+ofertaJSON.getJSONObject("author").get("id"));
+				response = client.execute(targetHost, companyRequest);
+				empresaJSON = new JSONObject(EntityUtils.toString(response.getEntity(), "UTF-8"));
+	
+				empresas.add(new Empresa(
+						empresaJSON.get("name").toString(), 
+						empresaJSON.get("description").toString(), 
+						empresaJSON.getJSONObject("typeIndustry").get("value").toString(),
+						Integer.parseInt(empresaJSON.get("numberWorkers").toString()),
+						empresaJSON.getJSONObject("country").get("value").toString()));
 			}
 			
-			// Para comprobar.
-			for(Oferta o: ofertas){
-				System.out.println(o.getNombre_oferta());
-				System.out.println(o.getNombre_empresa());
-				System.out.println(o.getLocalizacion());
-				System.out.println(o.getExperiencia());
-				System.out.println("-------------------------------");
+		}
+		
+		for(Oferta o: ofertas){
+			System.out.println(o.getNombre_oferta());
+			System.out.println(o.getNombre_empresa());
+			System.out.println(o.getLocalizacion());
+			System.out.println(o.getExperiencia());
+			System.out.println("-------------------------------");
 			}
+			
+		for(Empresa e: empresas){
+			System.out.println(e.getName());
+			System.out.println(e.getNumEmployers());
+			System.out.println("-------------------------------");
+		}
 			
 		} finally {
-			// release any connection resources used by the method
 			client.close();
 		}
 
